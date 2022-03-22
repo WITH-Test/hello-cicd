6257d80bfc017d74a77d7cbd156eee22e9f195e5
Carlos Gonzalez <carlos.gonzalez@with-madrid.com>
2022-03-22 10:17:59 +0100

    fix some linting errors

--- hello_aws/greet/models.py
+++
@@ -1,6 +0,0 @@
-"""
-Docstrings in modules are checked by pylint, in GitLab CI.
-"""
-from django.db import models
-
-# Create your models here.
--- hello_aws/greet/views.py
+++ hello_aws/greet/views.py
@@ -1,16 +1,10 @@
+"""
+Django views
+"""
 from django.http import JsonResponse
 
 # Create your views here.
 from django.views.decorators.http import require_http_methods
-
-
-
-
-
-
-
-
-
 
 
 @require_http_methods["GET"]
