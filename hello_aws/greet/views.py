8075f2f2afd510150dd8c8565df754a0a3b25643
Carlos Gonzalez <carlos.gonzalez@with-madrid.com>
2022-03-22 11:30:19 +0100

    parens were gone

--- hello_aws/greet/views.py
+++ hello_aws/greet/views.py
@@ -7,6 +7,6 @@ from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 
 
-@require_http_methods["GET"]
+@require_http_methods(["GET"])
 def hello_view(request):
     return JsonResponse({"message": f"Hello, CodeClimate?"})
