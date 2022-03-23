a90d061afcb62cb493a74090813dc3d4d5bfff89
Carlos Gonzalez <carlos.gonzalez@with-madrid.com>
2022-03-23 13:27:32 +0100

    make isort and black complain

--- hello_aws/greet/views.py
+++ hello_aws/greet/views.py
@@ -1,11 +1,8 @@
 """
 Django views
 """
-from django.http import JsonResponse
-
-# Create your views here.
 from django.views.decorators.http import require_http_methods
-
+from django.http import JsonResponse
 
 @require_http_methods(["GET"])
 def hello_view(request):
