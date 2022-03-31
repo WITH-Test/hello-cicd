66b45fcbf615e7e871c88e4f49d59f8f8076db89
Carlos Gonzalez <carlos.gonzalez@with-madrid.com>
2022-03-31 17:24:23 +0200

    changes in yell url

--- api/greet/urls.py
+++ api/greet/urls.py
@@ -4,5 +4,5 @@ from .views import hello_view, yell_view
 
 urlpatterns = [
     path(r"", hello_view, name="greet-people"),
-    path(r"loud", yell_view, name="yell-at-people"),
+    path(r"louder", yell_view, name="yell-at-people"),
 ]
